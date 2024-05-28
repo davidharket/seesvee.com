89a5b63a24e07c2c13179e74096712ba9b48ffd2
davidharket <davidharket123@gmail.com>
2024-05-28 18:01:01 +0200

    Test App

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div style={{ backgroundColor: 'white', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h1>Best of luck :)</h1>
+      <h1>Best of luck!</h1>
     </div>
   );
 }
