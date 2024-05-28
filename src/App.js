d96f2f2a1d5838377732f026bba6b601b2b7ede7
davidharket <davidharket123@gmail.com>
2024-05-28 17:59:22 +0200

    Update :)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-// src/App.js
-
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div style={{ backgroundColor: 'white', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>Best of luck :)</h1>
     </div>
   );
 }
 
 export default App;
+
