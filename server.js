07826b770c633019e6888d62ab70b787a51a0a68
davidharket <davidharket123@gmail.com>
2024-05-28 18:16:15 +0200

    Test change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the React app
+// Serve static files from the React app and more!
 app.use(express.static(path.join(__dirname, 'build')));
 
 // API routes
